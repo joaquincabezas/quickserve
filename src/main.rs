e8c80aae7117ebb7f29ce9496bc14de82ecd28af
joaquincabezas <1833086+joaquincabezas@users.noreply.github.com>
2024-10-29 17:45:51 +0100

    change

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let device = Device::Cpu;
 
     let a = Tensor::randn(0f32, 1., (2, 3), &device)?;
-    let b = Tensor::randn(0f32, 1., (3, 4), &device)?;
+    let b = Tensor::randn(0f32, 1., (3, 5), &device)?;
 
     let c = a.matmul(&b)?;
     println!("{c}");
